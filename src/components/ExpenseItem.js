af91bb4a0685201ac4d6b46ec22afd0b99131e85
jchoo0x <jchoo0x@icloud.com>
2023-08-18 11:03:10 +0900

    expenses.js

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,16 @@
+import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
-function ExpenseItem() {
-  const expenseDate = new Date(2023, 2, 28); // This is pure JavaScript code that has nothing to do with React
-  const expenseTitle = "Car Insurance";
-  const expenseAmount = 294.67;
+function ExpenseItem(props) {
+  // ExpenseDate is splitted from ExpenseItem -> There is no hard rule for splitting but
+  // the more the code becomes complex, the more the components become splitted
 
   return (
     <div className="expense-item">
-      <div>{expenseDate.toISOString()}</div>
+      <ExpenseDate />
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">${expenseAmount}</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </div>
   );
