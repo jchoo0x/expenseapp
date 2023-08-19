525dc49a644085b1c4b4b19702f35400d33e7508
jchoo0x <jchoo0x@icloud.com>
2023-08-19 14:20:39 +0900

    Dynamic adding expense lists

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 
 import "./ExpenseForm.css";
 
-const ExpenseForm = () => {
+const ExpenseForm = (props) => {
   const [enteredTitle, setEnteredTitle] = useState("");
   const [enteredAmount, setEnteredAmount] = useState("");
   const [enteredDate, setEnteredDate] = useState("");
@@ -19,12 +19,41 @@ const ExpenseForm = () => {
     setEnteredDate(event.target.value);
   };
 
+  // Alternative shared function
+  //   const inputChangeHandler = (identifier, value) => {
+  //     if (identifier === "title") {
+  //       setEnteredTitle(value);
+  //     } else if (identifier === "date") {
+  //       setEnteredDate(value);
+  //     } else if (identifier === "amount") {
+  //       setEnteredAmount(value);
+  //     }
+  //   };
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const expenseData = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate),
+    };
+
+    props.onSaveExpenseData(expenseData);
+    setEnteredTitle("");
+    setEnteredAmount("");
+    setEnteredDate("");
+  };
+
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" onChange={titleChangeHandler} />
+          <input
+            type="text"
+            value={enteredTitle} // Two-way binding; feed the state back to the input so when we change the state, we also change input
+            onChange={titleChangeHandler}
+          />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
@@ -32,6 +61,7 @@ const ExpenseForm = () => {
             type="number"
             min="0.01"
             step="0.01"
+            value={enteredAmount}
             onChange={amountChangeHandler}
           />
         </div>
@@ -41,11 +71,12 @@ const ExpenseForm = () => {
             type="date"
             min="2019-01-01"
             max="2022-12-31"
+            value={enteredDate}
             onChange={dateChangeHandler}
           />
         </div>
       </div>
-      <div classname="new-expense__actions">
+      <div className="new-expense__actions">
         <button type="submit">Add Expense</button>
       </div>
     </form>
